fa9bc2bb79d88d503ecd51c6af36165af6c12479
yakovalenko <jaroslawkovalenko@jmail.com>
2017-07-28 17:55:08 +0300

    format

--- ParseDb/src/ParseDb.java
+++ ParseDb/src/ParseDb.java
@@ -2,6 +2,7 @@
 import java.io.File;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -10,38 +11,40 @@ import org.w3c.dom.Element;
 public class ParseDb {
 
     public static void main(String[] args) {
-        
-        try{
 
-        File xmlFileToParse = new File("C:\\DATA\\ControllerDatabases\\BSC_339_db_11.03_ext.xml");
-        DocumentBuilderFactory dbFactory
-                = DocumentBuilderFactory.newInstance();
-        
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(xmlFileToParse);
-        doc.getDocumentElement().normalize();
-        
-        System.out.println("The object is:" + doc.getDocumentElement().getNodeName());
-        
-        NodeList nList  = doc.getElementsByTagName("managedObject");
-        
-        System.out.println("-----------------------------------");
-        
-        for (int temp = 0; temp < nList.getLength(); temp++) {
-            Node nNode = nList.item(temp);
-            System.out.println("\nCurrent Element :" + nNode.getNodeName());
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) nNode;
-                System.out.println("Object:" + eElement.getAttribute("managedObject"));
-                System.out.println("Object:" + eElement.getElementsByTagName("?").item(0).getTextContent());
-                System.out.println("Object:" + eElement.getElementsByTagName("?").item(0).getTextContent());
-                System.out.println("Object:" + eElement.getElementsByTagName("?").item(0).getTextContent());
-            }}
-        
-    
-} catch (Exception e) {
+        try {
 
-      e.printStackTrace();  
+            File xmlFileToParse = new File("C:\\DATA\\ControllerDatabases\\BSC_339_db_11.03_ext.xml");
+            DocumentBuilderFactory dbFactory
+                    = DocumentBuilderFactory.newInstance();
+
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFileToParse);
+            doc.getDocumentElement().normalize();
+
+            System.out.println("The object is:" + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("managedObject");
+
+            System.out.println("-----------------------------------");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Object:" + eElement.getAttribute("managedObject"));
+                    System.out.println("Object:" + eElement.getElementsByTagName("?").item(0).getTextContent());
+                    System.out.println("Object:" + eElement.getElementsByTagName("?").item(0).getTextContent());
+                    System.out.println("Object:" + eElement.getElementsByTagName("?").item(0).getTextContent());
+                }
+            }
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
     }
-    
-    }}
+}
