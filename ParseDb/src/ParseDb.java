9715df47d682c82b2a6f6e2389ac08c78edc3532
yakovalenko <jaroslawkovalenko@jmail.com>
2017-07-28 15:34:11 +0300

    add factory builder

--- ParseDb/src/ParseDb.java
+++ ParseDb/src/ParseDb.java
@@ -1,13 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author yakovalenko
- */
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 public class ParseDb {
+
+    public static void main(String[] args) {
+
+        File xmlFileToParse = new File("");
+        DocumentBuilderFactory dbFactory
+                = DocumentBuilderFactory.newInstance();
+    }
     
 }
