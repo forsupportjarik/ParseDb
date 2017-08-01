cd442e43201173f56bf37e584f0f844486b357b0
yakovalenko <jaroslawkovalenko@jmail.com>
2017-08-01 17:19:52 +0300

    arrays to string

--- ParseDb/src/Main.java
+++ ParseDb/src/Main.java
@@ -1,11 +1,12 @@
+import java.util.Arrays;
 
 public class Main {
     
     public static void main (String [] args) {
         
-        System.out.println(ParseDb.domTechniq(Utilities.accessDomDoc()));
+        System.out.println(Arrays.toString(ParseDb.domTechniq(Utilities.accessDomDoc())));
 
-        
+
     }
     
 }
