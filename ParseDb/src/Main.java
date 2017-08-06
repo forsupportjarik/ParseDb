7c2d2d02cf7dc670ecdbda3e71661c39d408c0e7
yakovalenko <jaroslawkovalenko@jmail.com>
2017-08-06 11:51:37 +0300

    get attr Name by <p Tag

--- ParseDb/src/Main.java
+++ ParseDb/src/Main.java
@@ -1,8 +1,17 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
     
-    public static void main (String [] args) {
+    public static void main (String [] args) throws IOException {
+
+
+        File file = new File("C:\\DATA\\Mentor projects\\ParseDb.txt");
+        FileWriter fileWriter = new FileWriter(file);
+
+        fileWriter.append(Arrays.toString(ParseDb.domTechniq(Utilities.accessDomDoc())).replace(",", "\n"));
         
         System.out.println(Arrays.toString(ParseDb.domTechniq(Utilities.accessDomDoc())).replace(",", "\n"));
 
--- ParseDb/src/ParseDb.java
+++ ParseDb/src/ParseDb.java
@@ -13,6 +13,8 @@ public class ParseDb {
 
         int lenght = nodeList.getLength();
 
+        String childTag = "";
+
         String[] result = new String[lenght];
 
         try {
@@ -28,22 +30,22 @@ public class ParseDb {
                         if (eElement.getNodeName().contains("managedObject")) {
 
 
-
                             String classObject = eElement.getAttribute("class");
                             String idObject = eElement.getAttribute("id");
                             String distName = eElement.getAttribute("distName");
                             String content = eElement.getTextContent();
 
-
-
                             NodeList nodeList1 = eElement.getElementsByTagName("p");
-                            Element element1 = (Element)  nodeList1.item(i);
-                            NodeList nodeList2 = element1.getChildNodes();
-                            System.out.println("NAME = " + (nodeList2.item(i)).getNodeValue());
-                            String s = element1.getAttribute("name");
-                            System.out.println("NAME = " + s );
 
-                           result[i] = classObject + ": " + idObject + " " + distName + " " + content.replace("\n", "\t");
+                            int contentLength = nodeList1.getLength();
+                            for (int j = 0; j < contentLength; j++) {
+                                Element eElement1 = (Element) nodeList1.item(j);
+                                childTag = eElement1.getAttribute("name");
+                            }
+
+
+                            result[i] = "|" + childTag + "|" + classObject + ": " + idObject + " " + distName + " " + content.replace("\n", "\t");
+
 
                         }
                     }
